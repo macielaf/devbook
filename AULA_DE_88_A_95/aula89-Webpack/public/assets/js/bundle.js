23a6d103554e6e89f4101a06159bbe1e50f61f2e
macielaf <macielaf@gmail.com>
2022-04-02 10:25:46 -0300

    Iniciando

---
+++ AULA_DE_88_A_95/aula89-Webpack/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+console.log("Olaaaa");
+//# sourceMappingURL=bundle.js.map
---
+++ AULA_DE_88_A_95/aula89-Webpack/webpack.config.js
@@ -0,0 +1,23 @@
+const path  = require('path'); // CommonJS
+
+module.exports = {
+    mode: 'production', // estamos trabalhando no desenvolvimento, arquivo menor mais rapido.development
+    entry: './src/index.js',
+    output: {
+       path: path.resolve(__dirname, 'public', 'assets', 'js'),
+       filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,  //qual arquivo quero testar, escapa o caractere de ponto. Vai ser usado.
+            use: {
+                loader: 'babel-loader', // o que quero usar. O que esta no packet.json no caso o babel que esta la
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }]
+    },
+    devtool: 'source-map'
+};
