1bf1c168b2b2e6135ad6a53add1971d945880b1d
macielaf <macielaf@gmail.com>
2022-04-28 14:42:06 -0300

    Iniciando

--- TESTe/routes.js
+++ TESTe/routes.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
-// Rotas da home
+// Rotas da home ok
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.trataPost);
 
